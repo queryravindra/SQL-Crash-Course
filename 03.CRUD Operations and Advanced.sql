92d5b37dfeb96703722161ea9ebaed6d362896e7
queryravindra <queryravindra14@gmail.com>
2021-11-06 09:49:15 +0530

    Initial commit

---
+++ 03.CRUD Operations and Advanced.sql
@@ -0,0 +1,131 @@
+CREATE DATABASE record_company;
+USE record_company;
+CREATE TABLE bands (
+	id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE albums (
+	id INT NOT NULL AUTO_INCREMENT,	
+    name VARCHAR(255) NOT NULL,
+    release_year INT,
+    band_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (band_id) REFERENCES bands(id)
+);
+
+-- Inserting Into table
+
+INSERT INTO bands (name)
+VALUES ('Iron Maiden');
+
+INSERT INTO bands (name)
+VALUES ('Deuce'), ('Avenged Sevenfold'), ('Ankor');
+
+-- Querying data from table
+
+SELECT * FROM bands;
+
+SELECT * FROM bands LIMIT 2;
+
+SELECT name FROM bands;
+
+-- Renaming the columns
+
+SELECT id AS 'ID', name AS 'Band Name' 
+FROM bands;
+
+SELECT * FROM bands ORDER BY name; -- default ASC
+
+SELECT * FROM bands ORDER BY name ASC;
+
+SELECT * FROM bands ORDER BY name DESC;
+
+-- -----------------------------------------------
+
+INSERT INTO albums (name, release_year, band_id)
+VALUES ('The Number of the Beasts', 1985, 1),
+	   ('Power Slave', 1984, 1),
+       ('Nightmare', 2018, 2),
+       ('Nightmare', 2010, 3),
+       ('Test Album', NULL, 3);
+       
+SELECT * FROM albums;
+
+SELECT name FROM albums;
+
+SELECT DISTINCT name FROM albums;
+
+UPDATE albums 
+SET release_year = 1982
+WHERE id = 1;
+       
+SELECT * FROM albums
+WHERE release_year < 2000;
+
+SELECT * FROM albums
+WHERE name LIKE '%er%';
+
+SELECT * FROM albums
+WHERE name LIKE '%er%' OR band_id = 2;
+
+SELECT * FROM albums
+WHERE release_year = 1984 and band_id = 1;
+
+SELECT * FROM albums
+WHERE release_year BETWEEN 2000 AND 2018;
+
+SELECT * FROM albums
+WHERE release_year IS NULL;
+
+DELETE FROM albums WHERE id = 5;
+       
+-- -------------------------------------------------------
+
+SELECT * FROM bands
+JOIN albums ON bands.id = albums.band_id;
+
+SELECT * FROM bands
+INNER JOIN albums ON bands.id = albums.band_id; -- same as above
+
+SELECT * FROM bands
+LEFT JOIN albums ON bands.id = albums.band_id;
+
+SELECT * FROM bands
+RIGHT JOIN albums ON bands.id = albums.band_id;
+
+SELECT * FROM albums
+RIGHT JOIN bands ON bands.id = albums.band_id;
+
+-- Aggregate Functions and GROUP BY
+
+SELECT AVG(release_year) FROM albums;
+
+SELECT SUM(release_year) FROM albums;
+
+SELECT band_id, COUNT(band_id) FROM albums
+GROUP BY band_id;
+
+SELECT b.name AS band_name, COUNT(a.id) AS num_albums
+FROM bands AS b
+LEFT JOIN albums AS a ON b.id = a.band_id
+GROUP BY b.id;
+
+-- SELECT b.name AS band_name, COUNT(a.id) AS num_albums
+-- FROM bands AS b
+-- LEFT JOIN albums AS a ON b.id = a.band_id
+-- WHERE num_albums = 1
+-- GROUP BY b.id; -- (won't work)
+
+SELECT b.name AS band_name, COUNT(a.id) AS num_albums
+FROM bands AS b
+LEFT JOIN albums AS a ON b.id = a.band_id
+GROUP BY b.id
+HAVING num_albums = 1;
+
+SELECT b.name AS band_name, COUNT(a.id) AS num_albums
+FROM bands AS b
+LEFT JOIN albums AS a ON b.id = a.band_id
+WHERE b.name = 'Deuce'
+GROUP BY b.id
+HAVING num_albums = 1;
